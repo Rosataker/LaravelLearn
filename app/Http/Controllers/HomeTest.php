1a7acaf6422edee0346614f7d870864a48d57ef0
Rosataker <pechin1219@gmail.com>
2017-03-24 19:28:43 +0800

    route learm

---
+++ app/Http/Controllers/HomeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeTest extends Controller
+{
+    //
+    public index function (){
+    	return 'Route::get(\'HomeTest\',\'HomeTest@index\');';
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -19,31 +19,88 @@ Route::get('/', function () {
 
 
 
-#取用app/Http/Controllers/HomeController.php 裡的public function
-//Route::get('/','HomeController@index');
 
-#Route::get('about','HomeController@index');
+#Route::get('about','HomeTest@index');
 Route::get('abou', function () {
    return 'Hello World abou';
 });
 
-Route::get('ggg', function () {
-   return 'Hello World ggggggggg';
+
+Route::get('/testPost',function(){
+    $csrf_token = csrf_token();
+    $form = <<<FORM
+        <form action="/hello" method="POST">
+            <input type="hidden" name="_token" value="{$csrf_token}">
+            <input type="submit" value="Test"/>
+        </form>
+FORM;
+    return $form;
+});
+
+Route::post('/hello',function(){
+    return "Hello Laravel[POST]!";
+});
+
+
+Route::get('testCsrf',function(){
+    $csrf_token = csrf_token();
+    $form = <<<FORM
+        <form action="/testPost" method="POST">
+          	<input type="hidden" name="_token" value="{$csrf_token}">
+            <input type="submit" value="Test"/>
+        </form>
+FORM;
+    return $form;
+});
+
+Route::post('testCsrf',function(){
+    return 'Success!';
 });
 
 
 
 /*
-Route::get('about', function () {
-    return view('welcome');
+Route::get('/hello/laravelacademy',['as'=>'academy',function(){
+    return 'Hello LaravelAcademy！';
+}]);
+
+Route::get('/testNamedRoute',function(){
+   #return route('academy');
+    return redirect()->route('academy');
 });
-*/
-/*
-Route::post('foo/bar', function()
-{
-    return 'Hello World';
+
+Route::get('/hello/laravelacademy/{id}',['as'=>'academy',function($id){
+    return 'Hello LaravelAcademy '.$id.'！';
+}]);
+
+Route::get('/testNamedRoute',function(){
+    return redirect()->route('academy',['id'=>1]);
 });
+
 */
 
-#php artisan make:controller HomeController 
-#利用artisan直接建立到app/Http/Controllers/
+Route::group(['as' => 'admin::'], function () {
+    Route::get('dashboard', ['as' => 'dashboard', function () {
+        //
+    }]);
+});
+
+
+Route::get('/testNamedRoute',function(){
+    return route('admin::dashboard');
+});
+
+#php artisan make:middleware TestMiddleware
+
+Route::group(['middleware'=>'test'],function(){
+    Route::get('/write/laravelacademy',function(){
+        //使用Test中间件
+    });
+    Route::get('/update/laravelacademy',function(){
+       //使用Test中间件
+    });
+});
+
+Route::get('/age/refuse',['as'=>'refuse',function(){
+    return "未成年人禁止入内！";
+}]);
